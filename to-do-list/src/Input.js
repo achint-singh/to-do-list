2cde408e4a8985c50de4ba66763e98af50877286
Achint Singh <achint@Achints-Air.attlocal.net>
2022-08-08 11:33:16 -0700

    Added edit functionality

--- to-do-list/src/Input.js
+++ to-do-list/src/Input.js
@@ -2,31 +2,86 @@ import React, {useState} from 'react';
 import { ListItem } from './ListItem';
 
 export const Input = () => {
-    const [item, setItem] = useState('');
+    const [inputItem, setInputItem] = useState({'name': '', 'editing': false, 'uuid': -1});
     const [items, setItems] = useState([]);
+    const [counter, setCounter] = useState(0);
     
     const handleChange = (event) => {
-        setItem(event.target.value)
+        let uuid = counter + 1;
+        setCounter(uuid);
+        let newItem = {
+            'name':event.target.value,
+            'editing': false,
+            'uuid': counter
+        };
+        setInputItem(newItem);
     }
 
     const onSubmit = (event) => {
         event.preventDefault();
-        setItem('');
-        setItems([...items, item]);
+        setItems([...items, inputItem]);
+        setInputItem({'name': '', 'editing': false, 'uuid': -1});
+    }
+
+    const editItem = (editingItem) => {
+        const updatedItems = items.map(i => {
+            if (i.uuid === editingItem.uuid) {
+                return {...i, 'editing': true, 'prevItem': editingItem.name};
+            }
+
+            return i;
+        });
+
+        setItems(updatedItems);
+    }
+
+    const onSave = (event, item) => {
+        event.preventDefault();
+        const updatedItems = items.map(i => {
+            if (i.uuid === item.uuid) {
+                return {...i, 'editing': false};
+            }
+
+            return i;
+        });
+        setItems([...updatedItems]);
+    }
+
+    const handleEditedInput = (event, item) => {
+        const updatedItems = items.map(i => {
+            if (i.uuid === item.uuid) {
+                return {...i, 'name': event.target.value};
+            }
+
+            return i;
+        });
+        setItems([...updatedItems]);
     }
 
     const deleteItem = (deletedItem) => {
-        let filteredItems = items.filter((item) => item !== deletedItem);
+        let filteredItems = items.filter((item) => item.uuid !== deletedItem.uuid);
         setItems([...filteredItems]);
+    }
+
+    const cancelEdit = (item) => {
+        const updatedItems = items.map(i => {
+            if (i.uuid === item.uuid) {
+                return {...i, 'name': item.prevItem, 'editing': false, 'prevItem': null};
+            }
+
+            return i;
+        });
+        setItems([...updatedItems]);
     }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input id='input' value={item} type="text" onChange={handleChange} />
+                <input id='input' value={inputItem.name} type="text" onChange={handleChange} />
                 <button>Submit</button>
             </form>
-            <ListItem items={items} deleteItem={deleteItem}/>
+            <ListItem items={items} deleteItem={deleteItem} editItem={editItem} onSave={onSave} 
+            handleEditedInput={handleEditedInput} cancelEdit={cancelEdit}/>
         </div>
     )
 }
--- to-do-list/src/ListItem.js
+++ to-do-list/src/ListItem.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import "./listItem.css";
 
 export const ListItem = (props) => {
-    const {items, deleteItem} = props;
+    const {items, deleteItem, editItem, onSave, handleEditedInput, cancelEdit} = props;
     const [checked, setChecked] = useState([]);
 
     const handleCheck = (event, item) => {
@@ -21,12 +21,24 @@ export const ListItem = (props) => {
 
     return (
         <div>
-            {items.map((item, index) => 
-            <div key={index}>
-                <input type="checkbox" id={item} name={item} onChange={(event) => handleCheck(event,item)}></input>
-                <label htmlFor={item} className={isChecked(item)}>{item}</label>
-                <button onClick={() => deleteItem(item)}>Delete</button>
-            </div>)}
+            {items.map((item, index) => {
+                if (item.editing) {
+                    return <div key={index}>
+                        <form onSubmit={(e) => onSave(e, item)}>
+                            <input id='edit-input' value={item.name} type="text" onChange={(e) => handleEditedInput(e, item)}/>
+                            <button type="button" onClick={() => cancelEdit(item)}>Cancel</button>
+                            <button type="submit">Save</button>
+                        </form>
+                    </div>
+                } else {
+                    return <div key={index}>
+                        <input type="checkbox" id={item.name} name={item.name} onChange={(e) => handleCheck(e,item.name)}></input>
+                        <label htmlFor={item.name} className={isChecked(item.name)}>{item.name}</label>
+                        <button onClick={() => editItem(item)}>Edit</button>
+                        <button onClick={() => deleteItem(item)}>Delete</button>
+                    </div>
+                }
+            })}
         </div>
     );
 }
